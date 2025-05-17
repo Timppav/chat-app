6393c486fc221389166d18d93fb2acc116f0733c
Timppav <tino.vuorela@gmail.com>
2025-05-17 04:59:46 +0300

    Socket.io connect and disconnect

--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on("connection", (socket) => {
+    console.log("User has joined!");
+
+    socket.on("disconnect", () => {
+        console.log("User has left!")
+    })
+});
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
