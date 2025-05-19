a4c9d96bc98b806e8c9d57a1977cb334924162af
Timppav <tino.vuorela@gmail.com>
2025-05-19 18:20:37 +0300

    Emoji picker

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import EmojiPicker from "emoji-picker-react";
+import { IoMdHappy } from "react-icons/io";
 
 import "./Input.css";
 
-const Input = ({ message, setMessage, sendMessage }) => (
-    <form className="form">
-        <input 
-            className="input"
-            type="text"
-            placeholder="Type a message..."
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
-            onKeyDown={event => event.key === "Enter" ? sendMessage(event) : null}
-        />
-        <button className="sendButton" onClick={(event) => sendMessage(event)}>Send</button>
-    </form>
-);
+const Input = ({ message, setMessage, sendMessage }) => {
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+
+    const onEmojiClick = (emojiData) => {
+        setMessage(prevMessage => prevMessage + emojiData.emoji);
+    };
+
+    const toggleEmojiPicker = (event) => {
+        event.preventDefault();
+        setShowEmojiPicker(prevState => !prevState);
+    }
+
+    return (
+        <form className="form">
+            <div className="inputContainer">
+                <div className="emojiPickerContainer">
+                    {showEmojiPicker && (
+                        <div className="emojiPicker">
+                            <EmojiPicker onEmojiClick={onEmojiClick} />
+                        </div>
+                    )}
+                </div>
+                <div className="inputWrapper">
+                    <button className="emojiButton" onClick={toggleEmojiPicker}><IoMdHappy size={20} /></button>
+                    <input 
+                        className="input"
+                        type="text"
+                        placeholder="Type a message..."
+                        value={message}
+                        onChange={(event) => setMessage(event.target.value)}
+                        onKeyDown={event => event.key === "Enter" ? sendMessage(event) : null}
+                    />
+                    <button className="sendButton" onClick={(event) => sendMessage(event)}>Send</button>
+                </div>
+            </div>
+        </form>
+    );
+};
 
 export default Input;
