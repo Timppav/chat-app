45cbce7e30fd8467e04eafb3cf15c70590125560
Timppav <tino.vuorela@gmail.com>
2025-05-19 18:31:52 +0300

    CSS improvements

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -16,7 +16,7 @@ const Message = ({ message: { user, text }, name }) => {
             ? (
                 <div className="messageContainer justifyEnd">
                     <p className="sentText pr-10">{trimmedName}</p>
-                    <div className="messageBox backgroundOrange">
+                    <div className="messageBox backgroundDark">
                         <p className="messageText colorWhite">{text}</p>
                     </div>
                 </div>
@@ -24,7 +24,7 @@ const Message = ({ message: { user, text }, name }) => {
             : (
                 <div className="messageContainer justifyStart">
                     <div className="messageBox backgroundLight">
-                        <p className="messageText colorDark">{text}</p>
+                        <p className="messageText colorWhite">{text}</p>
                     </div>
                     <p className="sentText pl-10">{user}</p>
                 </div>
