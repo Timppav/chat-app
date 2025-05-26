a44f9c4588d32367892f3805604d24ad0f55ba48
Timppav <tino.vuorela@gmail.com>
2025-05-26 19:06:36 +0300

    Close button styling

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,18 +1,33 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import closeIcon from "../../icons/closeIcon.png";
 
 import "./InfoBar.css";
 
-const InfoBar = ({ room }) => (
-    <div className="infoBar">
-        <div className="leftInnerContainer">
-            <h3>{room}</h3>
+const InfoBar = ({ room }) =>  {
+    const navigate = useNavigate();
+
+    const handleClose = () => {
+        navigate("/");
+    };
+
+    return (
+        <div className="infoBar">
+            <div className="leftInnerContainer">
+                <h3>{room}</h3>
+            </div>
+            <div className="rightInnerContainer">
+                <button
+                    type="button"
+                    className="closeButton"
+                    onClick={handleClose}
+                >
+                    <img className="closeIcon" src={closeIcon} alt="close" />
+                </button>
+            </div>
         </div>
-        <div className="rightInnerContainer">
-            <a href="/"><img src={closeIcon} alt="close" /></a>
-        </div>
-    </div>
-);
+    );
+}
 
 export default InfoBar;
